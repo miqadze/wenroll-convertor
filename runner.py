877b9d38b0f49ced50fbd4645a0cecad936e2020
miqadze <lasha.mikadze.1@btu.edu.ge>
2023-01-05 17:15:06 +0400

    transfer to flask

---
+++ runner.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def handle_request():
+    if 'video_name' in request.args:
+        video_name = request.args['video_name']
+        bucket_name = "production-wenroll"
+        url = "https://s3.eu-central-1.amazonaws.com/{}/{}".format(bucket_name, video_name)
+        content = requests.get(url).content
+        if content is None:
+            return "Error: File not found", 404
+        else:
+            with open(video_name, 'wb') as f:
+                f.write(content)
+            return "Process started", 200
+            subprocess.run(["python3", "convert.py", video_name])
+
+if __name__ == '__main__':
+    app.run()
