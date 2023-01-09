c3e84824c6c445ad633bb8adcc024d956c181e8a
miqadze <lasha.mikadze.1@btu.edu.ge>
2023-01-09 11:56:26 +0400

    file accept in flask

--- runner.py
+++ runner.py
@@ -4,19 +4,15 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def handle_request():
-    if 'video_name' in request.args:
-        video_name = request.args['video_name']
-        bucket_name = "development-wenroll"
-        url = "https://s3.eu-central-1.amazonaws.com/{}/{}".format(bucket_name, video_name)
-        content = requests.get(url).content
-        if content is None:
-            return "Error: File not found", 404
-        else:
-            with open(video_name, 'wb') as f:
-                f.write(content)
-            subprocess.Popen(["/usr/bin/python3", "convert.py", video_name])
-            return "Process started", 200
+    if 'video_key' in request.form and 'file' in request.files:
+        video_key = request.form['video_key']
+        file = request.files['file']
+        file.save(video_key)
+        subprocess.Popen(["/usr/bin/python3", "convert.py", video_key])
+        return "Process started", 200
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
