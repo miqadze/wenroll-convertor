f1b77b0ff529721951937d8d32f1f58fb61a9c72
miqadze <lasha.mikadze.1@btu.edu.ge>
2023-01-23 11:03:49 +0400

    merging from dev

--- s3.py
+++ s3.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import boto3
 import os
@@ -6,7 +7,13 @@ import subprocess
 import requests
 folder_name = sys.argv[1]
 s3 = boto3.client('s3')
-bucket_name = "production-wenroll"
+bucket_name = "development-wenroll"
+input_file = folder_name + "_720p.mp4"
+result = subprocess.run(["/usr/bin/ffmpeg", "-i", input_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output = result.stderr.decode()
+match = re.search(r"Duration: (\d{2}):(\d{2}):(\d{2})", output)
+duration_seconds = int(match.group(1)) * 3600 + int(match.group(2)) * 60 + int(match.group(3))
+print(f"Duration of {input_file} is {duration_seconds} seconds.")
 try:
  #   s3.upload_file(folder_name + "_480p.mp4", bucket_name, folder_name + "/" + folder_name + "_480p.mp4")
     s3.upload_file(folder_name + "_720p.mp4", bucket_name, folder_name + "/" + folder_name + "_720p.mp4")
@@ -19,14 +26,15 @@ try:
     s3.upload_file(folder_name + "_1080p.ts", bucket_name, folder_name + "/" + folder_name + "_1080p.ts")
     s3.upload_file(folder_name + "_playlist.m3u8", bucket_name, folder_name + "/" + folder_name + "_playlist.m3u8")
     s3.upload_file(folder_name + "_compressed.mp4", bucket_name, folder_name + "/" + folder_name + ".mp4")
+    subprocess.run(['aws', 's3', 'rm', 's3://development-wenroll/' + folder_name + '.mp4', '--recursive'])
 except Exception as e:
     print(f"Error occurred while running upload_file: {e}")
 try:
-    url = "https://api.wenroll.com/videos/conversion-success"
-    data = {"videoKey": folder_name + ".mp4"}
+    url = "https://apitest.wenroll.com/videos/conversion-success"
+    data = {"videoKey": folder_name + ".mp4", "duration": duration_seconds}
     response = requests.post(url, json=data)
     if response.status_code != 200:
-        print(f"Error occurred while sending PUT request: {response.text}")
+        print(f"Error occurred while sending POST request: {response.text}")
 except Exception as e:
     print(f"Error occured while writing in database: {e}")
 #os.remove(folder_name + "_480p.mp4")
