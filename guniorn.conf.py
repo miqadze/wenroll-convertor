23940829e9135d6653724f7c9c5409dc891083bb
miqadze <lasha.mikadze.1@btu.edu.ge>
2023-01-31 11:14:54 +0400

    fixes

---
+++ guniorn.conf.py
@@ -0,0 +1,4 @@
+workers = 4
+bind = "0.0.0.0:5000"
+chdir = "/var/www/html"
+module = "runner:app"
--- runner.py
+++ runner.py
@@ -1,31 +1,55 @@
-from flask import Flask, request, make_response
-import subprocess
+from flask import Flask, request, Request, Response, make_response
 import requests
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)
 
+class CheckAuthorization(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        auth_header = request.headers.get("Authorization")
+        video_key = request.args['video_key']
+
+        print("Authorizatin Header", auth_header)
+
+        if auth_header is None:
+            res = Response(u'Unauthorized', mimetype='text/plain', status=401)
+            print(res)
+            return res(environ, start_response)
+
+        headers = {"Authorization": auth_header, 'videoKey': video_key}
+        response = requests.post(
+            "https://apitest.wenroll.com/auth/validateToken", headers=headers)
+
+        response_data = response.json()
+
+        print("Token validation response", response_data)
+
+        if response_data['statusCode'] != 200:
+            res = Response(u'Unauthorized', mimetype='text/plain', status=401)
+            return res(environ, start_response)
+
+        return self.app(environ, start_response)
+app.wsgi_app = CheckAuthorization(app.wsgi_app)
+
 @app.route('/', methods=['PUT'])
 def handle_request():
-    auth_token = request.headers.get('Authorization')
-    if not auth_token:
-        return "Error: Authorization token missing", 401
-
     if 'video_key' in request.args and 'file' in request.files:
         video_key = request.args['video_key']
-        file = request.files['file']
-        # validate token
-        validate_token_response = requests.post('apitest.wenroll.com/validateToken', headers={'Authorization': auth_token})
-        if validate_token_response.status_code != 200:
-            return "Error: Token validation failed", validate_token_response.status_code
 
+        file = request.files['file']
         file.save(video_key)
         subprocess.Popen(["/usr/bin/python3", "convert.py", video_key])
         response = make_response("Process started", 200)
         return response
     else:
         return "Error: Invalid request", 400
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
